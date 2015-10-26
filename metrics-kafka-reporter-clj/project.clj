9922c227597cfd8ca62897e88e4dbeef212b1d7d
Adam Harper <github@adam-harper.com>
2015-10-26 10:57:43 +0000

    Version 0.2.0-SNAPSHOT

--- metrics-kafka-reporter-clj/project.clj
+++ metrics-kafka-reporter-clj/project.clj
@@ -1,4 +1,4 @@
-(defproject metrics-kafka-reporter/reporter-clj "0.1.0"
+(defproject metrics-kafka-reporter/reporter-clj "0.2.0-SNAPSHOT"
   :url "https://github.com/ah45/metrics-kafka-reporter"
   :description "A reporter for Codahale/Dropwizard Metrics that
     publishes measurements to an Apache Kafka topic as (by default)
@@ -9,4 +9,4 @@
             :distribution :repo}
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [metrics-kafka-reporter "0.1.0"]])
+                 [metrics-kafka-reporter "0.2.0-SNAPSHOT"]])
